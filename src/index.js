5fa0a96f7e682fa3cec6e8b33db7be7e151e8689
byusa <byusa.serge@gmail.com>
2021-11-27 17:36:16 -0800

    Finished the implementing redux

--- src/index.js
+++ src/index.js
@@ -10,19 +10,8 @@ import { Provider } from "react-redux";
 //STORE  -> Globalized STATE
 const store = createStore(
 	allReducer,
-	+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
-/*
-//Display it in the console.
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH (DISPATTCH this action to the reducer)
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(increment());
-
-*/
 
 ReactDOM.render(
 	<Provider store={store}>
